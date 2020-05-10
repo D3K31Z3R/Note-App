f57005b672f8431c356d62461a388ac3e1d7f81b
Benjamin Belderbos <61159439+D3K31Z3R@users.noreply.github.com>
2020-05-10 12:29:59 +0200

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,36 +9,36 @@ export class APIService {
   constructor(private http: HttpClient) { }
 
   GetUsers = () => {
-    return this.http.get('http://galvanized-obtainable-kilometer.glitch.me/users/');
+    return this.http.get('https://galvanized-obtainable-kilometer.glitch.me/users/');
   }
   GetUser = (name: string) => {
-    return this.http.get('http://galvanized-obtainable-kilometer.glitch.me/users?name=' + name);
+    return this.http.get('https://galvanized-obtainable-kilometer.glitch.me/users?name=' + name);
   }
   AddUser = (value: string) => {
-    return this.http.post('http://galvanized-obtainable-kilometer.glitch.me/users', {name: value});
+    return this.http.post('https://galvanized-obtainable-kilometer.glitch.me/users', {name: value});
   }
   AddNote = (name: string, category: string, note: string) => {
-    return this.http.post('http://galvanized-obtainable-kilometer.glitch.me/notes?name=' + name, {content: note, category: category});
+    return this.http.post('https://galvanized-obtainable-kilometer.glitch.me/notes?name=' + name, {content: note, category: category});
   }
   DeleteUser = (name: string) => {
-    return this.http.delete('http://galvanized-obtainable-kilometer.glitch.me/users?name=' + name);
+    return this.http.delete('https://galvanized-obtainable-kilometer.glitch.me/users?name=' + name);
   }
   DeleteNote = (id: number) => {
-    return this.http.delete('http://galvanized-obtainable-kilometer.glitch.me/notes?id=' + id);
+    return this.http.delete('https://galvanized-obtainable-kilometer.glitch.me/notes?id=' + id);
   }
   GetNotes = (name: string) => {
-    return this.http.get('http://galvanized-obtainable-kilometer.glitch.me/notes?name=' + name);
+    return this.http.get('https://galvanized-obtainable-kilometer.glitch.me/notes?name=' + name);
   }
   PostCategoy = (category: string) => {
-    return this.http.post('http://galvanized-obtainable-kilometer.glitch.me/categories', {name: category});
+    return this.http.post('https://galvanized-obtainable-kilometer.glitch.me/categories', {name: category});
   }
   GetCategories = () => {
-    return this.http.get('http://galvanized-obtainable-kilometer.glitch.me/categories');
+    return this.http.get('https://galvanized-obtainable-kilometer.glitch.me/categories');
   }
   UpdateNoteContent = (noteId: number, note: string) => {
-    return this.http.patch('http://galvanized-obtainable-kilometer.glitch.me/notes?id=' + noteId, {content: note});
+    return this.http.patch('https://galvanized-obtainable-kilometer.glitch.me/notes?id=' + noteId, {content: note});
   }
   UpdateNoteCategory = (noteId: number, name: string) => {
-    return this.http.patch('http://galvanized-obtainable-kilometer.glitch.me/notes?id=' + noteId, {category: name});
+    return this.http.patch('https://galvanized-obtainable-kilometer.glitch.me/notes?id=' + noteId, {category: name});
   }
 }
